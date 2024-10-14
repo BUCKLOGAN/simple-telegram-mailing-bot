6e2f5c024be1f66e0775bd1c41d992606bc75300
BUCKLOGAN <128492376+BUCKLOGAN@users.noreply.github.com>
2024-10-14 15:32:19 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TOKEN = 'YOUR_BOT_TOKEN'
-ADMIN_ID = 12345  # Замените на ID администратора
-USERS_FILE = 'users.txt'
+TOKEN = 'TOKEN' # Токен бота @BotFather
+ADMIN_ID = 12345  # ID администратора
+USERS_FILE = 'users.txt' # Файл с ID всех пользователей
